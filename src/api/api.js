ff031f611b708f4ffcae929cda51f9db4ad3cb9b
jayed969 <jayed.swe@gmail.com>
2023-01-02 16:52:02 +0600

    change api address

--- src/api/api.js
+++ src/api/api.js
@@ -2,8 +2,8 @@ import axios from 'axios'
 
 // Set config defaults when creating the instance
 export const bela = axios.create({
-    baseURL: 'http://188.166.250.49'
+    baseURL: 'http://157.245.204.157'
 });
 
 
-export const BASE_URL = 'http://188.166.250.49';
+export const BASE_URL = 'http://157.245.204.157';
